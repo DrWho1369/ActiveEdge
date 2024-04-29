69ea4dc6109bdbf5a6898057863315424c1bea86
Dr Who Baker <tbaker.bhb@gmail.com>
2024-04-29 20:43:01 +0100

    nav bar mobile first changes

--- index.js
+++ index.js
@@ -218,3 +218,19 @@ var images = document.querySelectorAll(".slide-content img");
 images.forEach(function (image) {
   image.addEventListener("click", enlargeImage);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the hamburger menu checkbox and the mobile nav element
+  var toggleMenuCheckbox = document.getElementById("toggle-menu");
+  var mobileNav = document.querySelector(".mobile-nav nav");
+
+  // Add event listener to the hamburger menu checkbox
+  toggleMenuCheckbox.addEventListener("change", function () {
+    // If the checkbox is checked, display the mobile nav; otherwise, hide it
+    if (this.checked) {
+      mobileNav.style.display = "block";
+    } else {
+      mobileNav.style.display = "none";
+    }
+  });
+});
